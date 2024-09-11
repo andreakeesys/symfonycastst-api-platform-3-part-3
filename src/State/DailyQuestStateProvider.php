0502a3d3f84dfc1605cb06258c434f4c3ba77176
Andrea Bergamasco <a.bergamasco@keesystem.com>
2024-09-11 15:21:05 +0200

    10. Custom Resource Item Provider

--- src/State/DailyQuestStateProvider.php
+++ src/State/DailyQuestStateProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\State;
 
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\DailyQuest;
@@ -14,7 +15,13 @@ class DailyQuestStateProvider implements ProviderInterface
 {
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        return $this->createQuests();
+        $dailyQuests = $this->createQuests();
+
+        if ($operation instanceof CollectionOperationInterface) {
+            return $dailyQuests;
+        }
+
+        return $dailyQuests[$uriVariables['dayString']] ?? null;
     }
 
     /**
