e11ff7dfc9875a0211727497671bfa88c09b45e4
Andrea Bergamasco <a.bergamasco@keesystem.com>
2024-09-13 16:24:47 +0200

    18. Provider: Transforming Entities to DTOs

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -4,7 +4,10 @@ namespace App\State;
 
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\UserApi;
+use App\Entity\User;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EntityToDtoStateProvider implements ProviderInterface
@@ -17,6 +20,30 @@ class EntityToDtoStateProvider implements ProviderInterface
     {
         $entities = $this->provider->provide($operation, $uriVariables, $context);
 
-        return $entities;
+        $dtos = [];
+        foreach ($entities as $entity) {
+            $dtos[] = $this->mapEntityToDto($entity);
+        }
+
+        return new TraversablePaginator(
+            new \ArrayIterator($dtos),
+            $entities->getCurrentPage(),
+            $entities->getItemsPerPage(),
+            $entities->getTotalItems(),
+        );
     }
+
+    private function mapEntityToDto(object $entity): object
+    {
+        /** @var User $entity */
+        $dto = new UserApi();
+        $dto->id = $entity->getId();
+        $dto->email = $entity->getEmail();
+        $dto->username = $entity->getUsername();
+        $dto->dragonTreasures = $entity->getDragonTreasures()->toArray();
+        $dto->flameThrowingDistance = rand(1, 10);
+
+        return $dto;
+    }
+
 }
