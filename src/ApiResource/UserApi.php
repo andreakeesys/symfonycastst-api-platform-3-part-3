5800361d037e844da1d4d38394a49d191b87466f
Andrea Bergamasco <a.bergamasco@keesystem.com>
2024-09-16 10:22:14 +0200

    24. DTO Validation & Security

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -7,14 +7,34 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Entity\DragonTreasure;
 use App\Entity\User;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            security: 'is_granted("PUBLIC_ACCESS")',
+            validationContext: ['groups' => ['Default', 'postValidation']]
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_USER_EDIT")',
+        ),
+        new Delete(),
+   ],
     paginationItemsPerPage: 5,
+    security: 'is_granted("ROLE_USER")',
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(entityClass: User::class)
@@ -27,12 +47,16 @@ class UserApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
+    #[NotBlank]
+    #[Email]
     public ?string $email = null;
 
+    #[NotBlank]
     public ?string $username = null;
 
     /** The plaintext password when set or changed */
     #[ApiProperty(readable: false)]
+    #[NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /** @var array<int, DragonTreasure> */
