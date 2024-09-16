b5dc98e4b01e207abeaf367e2db49932faf7de35
Andrea Bergamasco <a.bergamasco@keesystem.com>
2024-09-16 14:13:08 +0200

    33. Writable Relation Fields

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -76,6 +76,7 @@ class DragonTreasureResourceTest extends ApiTestCase
                     'description' => 'It sparkles when I wave it in the air.',
                     'value' => 1000,
                     'coolFactor' => 5,
+                    'owner' => '/api/users/' . $user->getId(),
                 ])
             )
             ->assertStatus(201)
